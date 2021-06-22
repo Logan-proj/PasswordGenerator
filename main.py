5af364855cf44b5cb6bb18f8c0ec94cd00cc008c
Logan-proj <79545798+Logan-proj@users.noreply.github.com>
2021-06-22 16:24:03 -0400

    Update main.py
    Added a loop so the user has the option of running the program again.

--- main.py
+++ main.py
@@ -4,35 +4,45 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Python Password Generator")
+print("\nPython Password Generator")
 print("-------------------------")
-password_length = int(input("Enter charachter length: "))
 
-password_list = []
+def password_generator():
+  password_length = int(input("Enter charachter length: "))
 
-#Desired amount of symbols & numbers <= 40% of string
-numberchar = int(password_length * .2)
-symbolchar = int(password_length * .2)
+  password_list = []
 
-#Randomly choosing & adding numbers to list
-for char in range(random.randint(1, numberchar)):
-  password_list.append(random.choice(numbers))
+  #Desired amount of symbols & numbers <= 40% of string
+  numberchar = int(password_length * .2)
+  symbolchar = int(password_length * .2)
 
-#Randomly choosing & adding symbols to list
-for char in range(random.randint(1, symbolchar)):
-  password_list.append(random.choice(symbols))
+  #Randomly choosing & adding numbers to list
+  for char in range(random.randint(1, numberchar)):
+    password_list.append(random.choice(numbers))
 
-#How many letters are needed to meet password length requirement
-letter_length = (password_length - len(password_list))
+  #Randomly choosing & adding symbols to list
+  for char in range(random.randint(1, symbolchar)):
+    password_list.append(random.choice(symbols))
 
-#Add random letters to end of password list
-password_list.extend(random.choices(letters, k=letter_length))
+  #How many letters are needed to meet password length requirement
+  letter_length = (password_length - len(password_list))
 
-random.shuffle(password_list)
+  #Add random letters to end of password list
+  password_list.extend(random.choices(letters, k=letter_length))
 
-#Turn list into string
-password = ""
-for char in password_list:
-  password += char
+  random.shuffle(password_list)
 
-print(f"Your generated password: {password}")
+  #Turn list into string
+  password = ""
+  for char in password_list:
+    password += char
+
+  print(f"Your generated password: {password}")
+
+run_program = True
+while run_program:
+  password_generator()
+  result = input("\nRun program again? Type Y for yes or N for no: ").lower()
+  if result == "n":
+    run_program = False
+    print("\nGoodbye")
